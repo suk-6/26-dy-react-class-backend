9d76276bb39df8d426b570ea7546796296b128ae
suk-6 <me@suk.kr>
2026-07-20 17:22:42 +0900

    deploy: change a port to 5000

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api-docs', app, swaggerDocument);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 void bootstrap();
